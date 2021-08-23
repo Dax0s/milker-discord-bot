72e9794b684520a53ae37badea1aaa8737cf02b0
Domas <klimaviciusdomas@gmail.com>
2021-08-23 14:19:51 +0300

    Add comments and move unused code to a different file.

--- bot.js
+++ bot.js
@@ -9,6 +9,8 @@ const prefix = '.';
 
 client.login(process.env.TOKEN);
 
+
+// Event listener which triggers when the discord bot logs in
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
     getInfo();
@@ -16,43 +18,24 @@ client.on("ready", () => {
 });
 
 
-const exampleEmbed = new MessageEmbed()
-	.setColor('#5531f5')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/AfFp7pu.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
-
-
+// Event listener which triggers when someone sends a message
 client.on("messageCreate", (message) => {
     let m_content = message.content;
-
+    
+    // Checks if message starts with the prefix and it wasn't sent by a bot
     if (!m_content.startsWith(prefix) || message.author.bot) return;
 
-    if (m_content.startsWith(prefix + "ping")) {
-        message.channel.send(message.author.tag);
-    }
-    else if (m_content.startsWith(prefix + 'username')) {
-        m_content = m_content.replace(`${prefix}username `, '');
-        message.client.user.setUsername(m_content);
-    } 
-    else if (m_content.startsWith(prefix + 'avatar')) {
+    // Sends the avatar of the user whom used the command
+    if (m_content.startsWith(prefix + 'avatar')) {
         message.channel.send(message.author.displayAvatarURL());
     }
+
+    // Activates the getInfo() function
     else if (m_content.startsWith(prefix + "get info")) {
         getInfo();
     }
+
+    // Bot sends a message to a channel different from where it got the message
     else if (m_content.startsWith(prefix + "fake channel"))
     {
         console.log(message);
@@ -62,40 +45,31 @@ client.on("messageCreate", (message) => {
         console.log('\n');
         message.channel.send("Yeaaaah booiiii");
     }
+
+    // Logs out the mentions in a message
     else if (m_content.startsWith(prefix + "check")) {
         console.log(message.mentions);
     }
-    else if (m_content.startsWith(prefix + "embed")) {
-        message.channel.send({ embeds: [exampleEmbed] });
-    }
 });
 
+// Event listener which triggers when a message is deleted
 client.on("messageDelete", (message) => {
     if (message.author.bot) return;
 
-    message.channel.send(`[${message.author.tag}] message was deleted: [${message.content}]`);
+    // Sends the deleted message or logs it out
+    // message.channel.send(`[${message.author.tag}] message was deleted: [${message.content}]`);
     console.log(`[${message.author.tag}] message was deleted: [${message.content}]`);
 });
 
+// Event listener which triggers when a message is updated
 client.on("messageUpdate", (oldMessage, newMessage) => {
-    oldMessage.channel.send(`[${oldMessage.author.tag}] edited a message: [${oldMessage}] => [${newMessage}]`);
+    if (message.author.bot) return;
+
+    // Sends the updated message or logs it out
+    // oldMessage.channel.send(`[${oldMessage.author.tag}] edited a message: [${oldMessage}] => [${newMessage}]`);
     console.log(`[${oldMessage.author.tag}] edited a message: [${oldMessage}] => [${newMessage}]`);
 });
 
 function getInfo() {
     console.log(client.guilds);
 }
-
-
-
-/* async function replyWithInvite(message) {
-    let invite = await message.channel.createInvite(
-    {
-      maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
-      maxUses: 1 // maximum times it can be used
-    },
-  )
-  .catch(console.log);
-  
-    console.log(invite ? `Here's your invite: ${invite}` : "There has been an error during the creation of the invite.");
-  } */
