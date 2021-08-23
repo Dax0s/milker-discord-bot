f19b6eb680f6512ca86248c7e4f08059efba5742
Domas <klimaviciusdomas@gmail.com>
2021-08-23 20:58:06 +0300

    Add "Kicked successfully" and "Kick failed" embeds.

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const embeds = require('./embeds');
+
 require('dotenv').config();
 
 const discord = require('discord.js');
@@ -5,13 +7,13 @@ const { MessageEmbed } = require('discord.js');
 
 const client = new discord.Client({ intents: ['GUILD_MESSAGES', 'GUILDS'] });
 
-const PREFIX = '.';
+const PREFIX = process.env.PREFIX;
 
 client.login(process.env.TOKEN);
 
 
 // Event listener which triggers when the discord bot logs in
-client.on("ready", () => {
+client.on('ready', () => {
     console.log('\n' + `Logged in as ${client.user.tag}!` + '\n');
     getInfo();
     console.log('\n');
@@ -19,7 +21,7 @@ client.on("ready", () => {
 
 
 // Event listener which triggers when someone sends a message
-client.on("messageCreate", (message) => {
+client.on('messageCreate', (message) => {
     if (message == null) return;
 
     // Checks if message starts with the prefix and it wasn't sent by a bot
@@ -33,52 +35,58 @@ client.on("messageCreate", (message) => {
     console.log(command, args, '\n');
 
     // Sends the avatar of the user whom used the command
-    if (command === "avatar") {
+    if (command === 'avatar') {
         message.channel.send(message.author.displayAvatarURL());
     }
 
     // Activates the getInfo() function
-    else if (command === "getInfo") {
+    else if (command === 'getInfo') {
         console.log(message.guild.members.fetch('483377420494176258'));
     }
 
+    // Command to test random things
+    else if (command === 'test') {
+        message.channel.send({ embeds: [embeds.kickHelp] });
+    }
+
     // Bot sends a message to a channel different from where it got the message
-    else if (command === "fakeChannel")
+    else if (command === 'fakeChannel')
     {
         console.log(message);
         console.log('\n');
         message.channelId = '878648104864346112';
         console.log(message);
         console.log('\n');
-        message.channel.send("Yeaaaah booiiii");
+        message.channel.send('Yeaaaah booiiii');
     }
 
     // Logs out the mentions in a message
-    else if (command === "checkMentions") {
+    else if (command === 'checkMentions') {
         console.log(message.mentions);
     }
 
     // Kicks the member with the specified ID
-    else if (command === "kick") {
+    else if (command === 'kick') {
         const authorMemberObj = message.guild.members.cache.get(message.author.id);
-        if (!authorMemberObj.permissions.has('KICK_MEMBERS')) return message.reply("You do not have the permissions to kick someone");
+        if (!authorMemberObj.permissions.has('KICK_MEMBERS')) return message.reply('You do not have the permissions to kick someone');
 
-        if (args.length === 0) return message.reply("Please provide an ID or mention someone");
+        if (args.length === 0) return message.reply('Please provide an ID or mention someone');
         args[0] = args[0].replace(/[^0-9]/g, '');
 
         const members = message.guild.members;
+        const member = members.cache.get(args[0]);
         if (members) {
             members.kick(args[0])
-                .then((member) => message.channel.send(`${member} was kicked`))
-                .catch((err) => message.channel.send(`Couldn't kick ${member}`));
+                .then((member) => message.channel.send({ embeds: [embeds.kickSucceded.setDescription(`${embeds.successEmote} ${member} was kicked successfully`)] }))
+                .catch((err) => message.channel.send({ embeds: [embeds.kickFailed] }));
         } else {
-            message.reply("Member not found");
+            message.reply('Member not found');
         }
     }
 });
 
 // Event listener which triggers when a message is deleted
-client.on("messageDelete", (message) => {
+client.on('messageDelete', (message) => {
     if (message.author.bot) return;
 
     // Sends the deleted message or logs it out
@@ -87,8 +95,8 @@ client.on("messageDelete", (message) => {
 });
 
 // Event listener which triggers when a message is updated
-client.on("messageUpdate", (oldMessage, newMessage) => {
-    if (message.author.bot) return;
+client.on('messageUpdate', (oldMessage, newMessage) => {
+    if (oldMessage.author.bot) return;
 
     // Sends the updated message or logs it out
     console.log(`[${oldMessage.author.tag}] edited a message: [${oldMessage}] => [${newMessage}]`);
---
+++ embeds.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const { MessageEmbed } = require('discord.js');
+
+const PREFIX = process.env.PREFIX;
+
+const errorEmote = '<:error:879405644149571645>';
+const successEmote = '<:success:879405668778520606>';
+
+const exampleEmbed = new MessageEmbed()
+	.setColor('#5531f5')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+const kickFailed = new MessageEmbed()
+    .setColor('#CC0101')
+    .setDescription(`${errorEmote} couldn't kick the user`);
+
+const kickSucceded = new MessageEmbed()
+    .setColor('#42B482')
+    .setDescription(`${successEmote} user kicked successfully`);
+
+const kickHelp = new MessageEmbed()
+    .setColor('#dae5f0')
+    .setTitle(`Command: ${PREFIX}kick`)
+    .addField('Description: ', 'Kick a member', false);
+    // .setFields(
+    //     { name: 'Description: ', value: 'Kick a member', inline: false },
+    //     { name: 'Usage', value: `${PREFIX}kick [user]`, inline: false }
+    // );
+
+module.exports.example = exampleEmbed;
+module.exports.kickFailed = kickFailed;
+module.exports.kickSucceded = kickSucceded;
+module.exports.kickHelp = kickHelp;
+
+module.exports.successEmote = successEmote;
